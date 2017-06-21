a0d84ee0b61ef7cf41ddde9b7a962636c3ef7125
Krishna Lingashetty <lkc0987@gmail.com>
2017-06-21 00:05:10 -0700

    basic store

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,6 +1,19 @@
 import * as ChatHeader from "./components/chat-header.vue";
 import * as ChatApp from "./components/chat-app.vue";
 
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+        count: 0
+    },
+    mutations: {
+        increment (state) {
+            state.count++
+        }
+    }
+});
+
 new Vue({
     el: '.app-container',
     components: {
